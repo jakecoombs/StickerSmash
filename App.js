4186ee7c9d2b50f11d51d61c1bb82c5570b7e7e9
jakecoombs <57802613+jakecoombs@users.noreply.github.com>
2024-02-25 11:20:29 +0000

    Update style

--- App.js
+++ App.js
@@ -2,18 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    return (
+      <View style={styles.container}>
+        <Text style={{ color: '#fff' }}>
+          Open up App.js to start working on your app!
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
   },
