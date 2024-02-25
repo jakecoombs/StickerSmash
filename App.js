3cdfe71db7dc9caa6549dc3093bd365f92b71e84
jakecoombs <57802613+jakecoombs@users.noreply.github.com>
2024-02-25 11:20:56 +0000

    Add image

--- App.js
+++ App.js
@@ -1,22 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
+
+const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
-    return (
-      <View style={styles.container}>
-        <Text style={{ color: '#fff' }}>
-          Open up App.js to start working on your app!
-        </Text>
-        <StatusBar style="auto" />
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
       </View>
-    );
-  }
+      <StatusBar style="auto" />
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
 });
